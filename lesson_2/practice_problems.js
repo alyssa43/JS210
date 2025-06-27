5b993b31c5893413ee64f6867eeaf21a8a9d08cd
Alyssa Easter <AlyssaStarley@gmail.com>
2025-06-27 14:37:18 -0700

    Began Lesson 2

---
+++ lesson_2/practice_problems.js
@@ -0,0 +1,18 @@
+function average(arr) {
+  return sum(arr) / arr.length;
+}
+
+function sum(arr) {
+  let total = 0;
+  for (i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
+
+console.log(average([1, 2, 3, 4, 5]));
+console.log(average([10, 20, 30, 40, 50]));
+console.log(average([100, 200, 300, 400, 500]));
+
+let temperatures = [98, 72, 102, 81, 63];
+console.log(average(temperatures)); 
