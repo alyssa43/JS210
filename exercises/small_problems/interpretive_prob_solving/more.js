49d24c3deae241f1ee2b965450b6d8e770b67577
Alyssa Easter <100172781+alyssa43@users.noreply.github.com>
2025-11-13 10:19:57 -0800

    Worked on more practice problems

--- exercises/small_problems/interpretive_prob_solving/more.js
+++ exercises/small_problems/interpretive_prob_solving/more.js
@@ -274,27 +274,92 @@ ending: string
 3. Return `status`
 */
 
-function likeOrDislike(buttons) {
-  let status = "Nothing";
-  buttons.forEach(button => status = status === button ? "Nothing" : button);
-  return status;
-}
+// function likeOrDislike(buttons) {
+//   let status = "Nothing";
+//   buttons.forEach(button => status = status === button ? "Nothing" : button);
+//   return status;
+// }
 
-console.log(likeOrDislike(["Like"])); // "Like"
-console.log(likeOrDislike(["Dislike", "Like"])); // "Like"
-console.log(likeOrDislike(["Like", "Like", "Like"])); // "Like"
-console.log(likeOrDislike(["Dislike", "Dislike", "Like", "Like", "Dislike", "Like"])); // "Like"
-console.log(likeOrDislike(["Like", "Like", "Like", "Like", "Like", "Like", "Like"])); // "Like"
+// console.log(likeOrDislike(["Like"])); // "Like"
+// console.log(likeOrDislike(["Dislike", "Like"])); // "Like"
+// console.log(likeOrDislike(["Like", "Like", "Like"])); // "Like"
+// console.log(likeOrDislike(["Dislike", "Dislike", "Like", "Like", "Dislike", "Like"])); // "Like"
+// console.log(likeOrDislike(["Like", "Like", "Like", "Like", "Like", "Like", "Like"])); // "Like"
 
-console.log(likeOrDislike(["Dislike"])); // "Dislike"
-console.log(likeOrDislike(["Dislike"])); // "Dislike"
-console.log(likeOrDislike(["Dislike", "Dislike", "Dislike"])); // "Dislike"
-console.log(likeOrDislike(["Dislike", "Dislike", "Like", "Like", "Dislike", "Like", "Dislike"])); // "Dislike"
-console.log(likeOrDislike(["Dislike", "Dislike", "Dislike", "Dislike", "Dislike", "Dislike", "Dislike"])); // "Dislike"
+// console.log(likeOrDislike(["Dislike"])); // "Dislike"
+// console.log(likeOrDislike(["Dislike"])); // "Dislike"
+// console.log(likeOrDislike(["Dislike", "Dislike", "Dislike"])); // "Dislike"
+// console.log(likeOrDislike(["Dislike", "Dislike", "Like", "Like", "Dislike", "Like", "Dislike"])); // "Dislike"
+// console.log(likeOrDislike(["Dislike", "Dislike", "Dislike", "Dislike", "Dislike", "Dislike", "Dislike"])); // "Dislike"
 
-console.log(likeOrDislike(["Like", "Like"])); // "Nothing"
-console.log(likeOrDislike(["Like", "Dislike", "Dislike"])); // "Nothing"
-console.log(likeOrDislike([])); // "Nothing"
-console.log(likeOrDislike(["Dislike", "Dislike", "Like", "Like", "Dislike", "Dislike"])); // "Nothing"
-console.log(likeOrDislike(["Dislike", "Dislike", "Like", "Like", "Like", "Like"])); // "Nothing"
-console.log(likeOrDislike(["Dislike", "Dislike", "Dislike", "Dislike", "Dislike", "Dislike"])); // "Nothing"
+// console.log(likeOrDislike(["Like", "Like"])); // "Nothing"
+// console.log(likeOrDislike(["Like", "Dislike", "Dislike"])); // "Nothing"
+// console.log(likeOrDislike([])); // "Nothing"
+// console.log(likeOrDislike(["Dislike", "Dislike", "Like", "Like", "Dislike", "Dislike"])); // "Nothing"
+// console.log(likeOrDislike(["Dislike", "Dislike", "Like", "Like", "Like", "Like"])); // "Nothing"
+// console.log(likeOrDislike(["Dislike", "Dislike", "Dislike", "Dislike", "Dislike", "Dislike"])); // "Nothing"
+
+// ---- PROBLEM 4 ----
+// This challenge concerns square matrices (same number of rows and columns) as the below example illustrates:
+// [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ]
+// The entries in the diagonal line from the top left to the bottom right form the main diagonal of the matrix. In this case, 1,5,9 form the main diagonal.
+// Write a function that returns the matrix obtained by replacing the entries above the main diagonal with 0s.
+// For example, for the matrix above you should return:
+// [
+//   [1, 0, 0],
+//   [4, 5, 0],
+//   [7, 8, 9]
+// ]
+// Notes
+// As in the examples, the size of the matrices will vary (but they will always be square).
+// In Linear Algebra, matrices with 0s above the diagonal are called lower triangular matrices.
+
+/*
+---- PROBLEM ----
+input: a nested array representing a nxn matrix
+output: a nested array --> the transformed matrix
+
+Rules:
+  --> The argument will always be an array
+  --> If the given array is empty; return an empty array
+  --> Will the given array subarrays only contain numbers? No, it can contain any other non-primitive value
+  --> The matrix will always be sqaure(EX: array length === all subarrays length)
+  --> Only transform "above" the diagonal
+
+Definitions:
+  --> Matrix: an n X n grid
+  --> The diagonal: an imaginary line from top left to bottom right (EX: arr[0][0], arr[1][1], arr[2][2]...)
+
+*/
+
+console.log(lowerTriang([[1, 2, 3], [4, 5, 6], [7, 8, 9]])); 
+// [[1, 0, 0],[4, 5, 0], [7, 8, 9]]
+
+console.log(lowerTriang([[5, 7], [7, 9]])); 
+// [[5, 0], [7, 9]]
+
+console.log(lowerTriang([[1, 8, 8, 1], [2, 7, 7, 2], [3, 6, 6, 3],[4, 5, 5, 4]])); 
+// [[1, 0, 0, 0], [2, 7, 0, 0], [3, 6, 6, 0], [4, 5, 5, 4]]
+
+
+
+
+//// SPOT SESSION ////
+
+// Create a function that takes three arrays and returns one array where all passed arrays are combined into nested arrays.
+
+// These arrays should be combined based on indexes: the first nested array should contain only the items on index 0, the second array on index 1, and so on.
+
+// If any array contains fewer items than necessary, supplement the missing item with "*".
+
+// Examples
+
+// combineArrays([false, "false"], ["true", true, "bool"], ["null", "undefined"]) ➞ [[false, "true", "null"], ["false", true, "undefined"], ["*", "bool", "*"]]
+
+// combineArrays([1, 2, 3], [4, 5, 6], [7, 8, 9]) ➞ [[1, 4, 7], [2, 5,  8], [3, 6, 9]]
+
+// combineArrays(["Jack", "Joe", "Jill"], ["Stuart", "Sammy", "Silvia"], ["Rick", "Raymond", "Riri"]) ➞ [["Jack", "Stuart", "Rick"], ["Joe", "Sammy",  "Raymond"], ["Jill", "Silvia", "Riri"]]
